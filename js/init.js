4d0fbb647c024eb1aa602cbb56432bccda9a424d
Andres <andres_movilla1103@hotmail.com>
2017-03-31 19:00:48 -0500

    added carousel

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 (function($){
   $(function(){
     $('.parallax').parallax();
+    $('.carousel').carousel({indicators: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
