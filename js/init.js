cfd071b0e0e42e2d83650831cfa157d38aafb44d
Echelan <andres_movilla1103@hotmail.com>
2017-08-16 10:53:01 -0500

    removed git page

--- js/init.js
+++
@@ -1,6 +0,0 @@
-(function($){
-  $(function(){
-    $('.parallax').parallax();
-    $('.carousel').carousel({indicators: true});
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
